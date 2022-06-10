67169b6a60a24110fe195ad6b402017020e34da0
Amit Gorai <amit.gorai16@gmail.com>
2022-06-10 14:59:04 +0530

    My Contact Form all data save into Formspree Account. Add thsi Function

--- index.js
+++ index.js
@@ -1,68 +1,72 @@
-// !form validation
+//            // !form validation
 
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const Pnumber = document.getElementById('Pnumber')
-const subject = document.getElementById('subject')
-const textarea = document.getElementById('textarea')
+// const form = document.getElementById('form')
+// const username = document.getElementById('username')
+// const email = document.getElementById('email')
+// const Pnumber = document.getElementById('Pnumber')
+// const subject = document.getElementById('subject')
+// const textarea = document.getElementById('textarea')
 
-// !Add event
+//             // !Add event
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    Validate();
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     Validate();
 
-})
+// })
 
-// !Define Validate Funcation
+//              // !Define Validate Funcation
 
-const Validate = () => {
-    const usernameVal = username.value.trim();
-    const emailVal = email.value.trim();
-    const PnumberVal = Pnumber.value.trim();
-    const subjectVal = subject.value.trim();
-    const textareaVal = textarea.value.trim();
+// const Validate = () => {
+//     const usernameVal = username.value.trim();
+//     const emailVal = email.value.trim();
+//     const PnumberVal = Pnumber.value.trim();
+//     const subjectVal = subject.value.trim();
+//     const textareaVal = textarea.value.trim();
 
-    //?validate username
+//     //?validate username
 
-    if (usernameVal === " ") {
-        setErrorMsg(usernameVal, 'username cannot ne blank');
-    }
-    else if (usernameVal.length <= 2) {
-        setErrorMsg(usernameVal, 'usename min 3 charactor');
-    }
-    else {
-        setSuccessMsg(usernameVal);
-    }
-
-
+//     if (usernameVal === " ") {
+//         setErrorMsg(usernameVal, 'username cannot ne blank');
+//     }
+//     else if (usernameVal.length <= 2) {
+//         setErrorMsg(usernameVal, 'usename min 3 charactor');
+//     }
+//     else {
+//         setSuccessMsg(usernameVal);
+//     }
 
 
-     //!more email validation
 
-    const isEmail =(emailVal)=>{
-        var atSymbol = emailVal.indexOf("@");
-        if(atSymbol < 1)return false;
 
-        var dot=emailVal.lastIndexof('.');
-        if(dot <= atSymbol+2)return false;
-        if(dot=== emailVal.length-1)return false;
-        return true;
-    }
+//      //!more email validation
 
-    //?validate Email
+//     const isEmail =(emailVal)=>{
+//         var atSymbol = emailVal.indexOf("@");
+//         if(atSymbol < 1)return false;
 
-    if (emailVal === " ") {
-        setErrorMsg(emailVal, 'email cannot ne blank');
-    }
-    else if (!isEmail(emailVal) ) {
-        setErrorMsg(emailVal, 'Not a valid email');
-    }
-    else {
-        setSuccessMsg(emailVal);
-    }
-}
+//         var dot=emailVal.lastIndexof('.');
+//         if(dot <= atSymbol+2)return false;
+//         if(dot=== emailVal.length-1)return false;
+//         return true;
+//     }
+
+//     //?validate Email
+
+//     if (emailVal === " ") {
+//         setErrorMsg(emailVal, 'email cannot ne blank');
+//     }
+//     else if (!isEmail(emailVal) ) {
+//         setErrorMsg(emailVal, 'Not a valid email');
+//     }
+//     else {
+//         setSuccessMsg(emailVal);
+//     }
+// }
+
+// function setErrorMsg(input, errormsgs){
+
+// }
 
 
 
